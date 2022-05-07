632236fcdf9d3f889a3608c507a2f65ed8d25d1f
yossy1190 <69608729+yossy1190@users.noreply.github.com>
2022-05-07 12:29:34 +0900

    Update kimuland_20220504.py

--- kimuland_20220504.py
+++ kimuland_20220504.py
@@ -1,9 +1,11 @@
+import enum
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 import openpyxl
+import xlwings
 
 def set_driver(hidden_chrome: bool=False):
     USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
@@ -22,15 +24,20 @@ def set_driver(hidden_chrome: bool=False):
 wb=openpyxl.load_workbook("kimuland_20220505.xlsx")
 wsinfo=wb['検索情報']
 wsresult=wb["検索結果"]
+
+# 検索情報を読み込み
 amount=wsinfo.cell(4,11).value
 from_finprc=wsinfo.cell(4,12).value
 to_finprc=wsinfo.cell(4,13).value
 from_yes=wsinfo.cell(4,14).value
 to_yes=wsinfo.cell(4,15).value
-    # print(amount,from_finprc,to_finprc,from_yes,to_yes)
+
+# 検索結果シートの最終行を取得
+Sheet_Max=wsresult.max_row+1
 
 def main():
     driver=set_driver()
+    print("実行中です。10秒程度お待ちください")
     url="https://www.traders.co.jp/market_jp/screening"
     driver.get(url)
     driver.find_element(by=By.CSS_SELECTOR,value=("#flg_sel03")).click()
@@ -41,17 +48,26 @@ def main():
     driver.find_element(by=By.CSS_SELECTOR,value=("#change_price_min")).send_keys(from_yes)
     driver.find_element(by=By.CSS_SELECTOR,value=("#change_price_max")).send_keys(to_yes)
     driver.find_element(by=By.CSS_SELECTOR,value=("#submit_btn")).click()
-    time.sleep(2)
- 
-    tbody=driver.find_elements(by=By.TAG_NAME,value="tbody")
-    elems=tbody[2].find_elements(by=By.TAG_NAME,value=("td"))
-
-    for i,elem in enumerate(elems,1):
-        if i<9:
-            wsresult.cell(row=rows,column=1+i).value=elem.text
-        elif i>=9:
-            i=
-             
+    time.sleep(1)
+    # 検索結果が０件だったら、なしと記入したい。
+    trs = driver.find_elements(by=By.CSS_SELECTOR, value="table.data_table > tbody > tr") 
+    for r,tr in enumerate(trs):
+        tds=tr.find_elements(by=By.CSS_SELECTOR,value="td")
+        for c,td in enumerate(tds):
+            wsresult.cell(row=r+4,column=c+2).value=td.text                
+            wsresult.cell(row=r+4,column=c+2).value=td.text            
+        wsresult.cell(row=r+4,column=1).value=amount            
+    
+    # 空白行を削除
+    for bi in reversed(range(4,Sheet_Max)):
+        GetValue1=wsresult.cell(row=bi,column=4).value
+        if GetValue1==None:
+            wsresult.delete_rows(bi)
+        else:
+            Getvalue2=GetValue1.strip()
+            if len(Getvalue2)==0:
+                wsresult.delete_rows(bi)
+    
     wb.save("kimuland_20220505.xlsx")
 
 main()
